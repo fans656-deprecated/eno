82f7a2cca02240331eb56426d97beb8093f512ad
fans656 <fans656@yahoo.com>
2015-04-12 16:28:01 +0800

    Next: show text
    Done:
    1. frameless window
    2. window title
    3. dock at right-bottom corner
    4. background color
    5. (1px) border

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from PySide.QtCore import *
 from widget import Widget
 
 app = QApplication(sys.argv)
+availGeo = QApplication.desktop().availableGeometry()
+
 w = Widget()
 w.show()
+w.resize(640, 480)
+w.move(availGeo.width() - w.width(), availGeo.height() - w.height())
+
 app.exec_()
--- widget.py
+++ widget.py
@@ -1,7 +1,26 @@
 from PySide.QtGui import *
 from PySide.QtCore import *
 
+styleSheet = '''
+QWidget {
+    background: #002B36;
+}
+'''
+
+text = open(r'main.py').read().decode('utf-8')
+
 class Widget(QDialog):
 
     def __init__(self, parent=None):
-        super(Widget, self).__init__(parent)
+        super(Widget, self).__init__(parent, Qt.FramelessWindowHint)
+        self.setWindowTitle('eno')
+        self.setStyleSheet(styleSheet)
+
+    def paintEvent(self, ev):
+        p = QPainter(self)
+        pen = p.pen()
+        pen.setColor(QColor('#C4D2FF'))
+        p.setPen(pen)
+        p.drawRect(self.rect().adjusted(0, 0, -1, -1))
+
+        p.drawText(100, 100, text)
