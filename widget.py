21e8eb896def36fad6fde94283109d6b92bac04e
fans656 <fans656@yahoo.com>
2015-04-12 18:58:12 +0800

    Show text

--- widget.py
+++ widget.py
@@ -8,6 +8,7 @@ QWidget {
 '''
 
 text = open(r'main.py').read().decode('utf-8')
+font = QFont('Consolas', 11)
 
 class Widget(QDialog):
 
@@ -15,12 +16,23 @@ class Widget(QDialog):
         super(Widget, self).__init__(parent, Qt.FramelessWindowHint)
         self.setWindowTitle('eno')
         self.setStyleSheet(styleSheet)
+        self.doc = QTextDocument()
+
+        fmt = QTextCharFormat()
+        fmt.setForeground(QBrush(QColor('#7C8E91')))
+        fmt.setFont(font)
+
+        c = QTextCursor(self.doc)
+        c.insertText(text, fmt)
 
     def paintEvent(self, ev):
         p = QPainter(self)
+
+        # draw window border
         pen = p.pen()
         pen.setColor(QColor('#C4D2FF'))
         p.setPen(pen)
         p.drawRect(self.rect().adjusted(0, 0, -1, -1))
 
-        p.drawText(100, 100, text)
+        # draw text
+        self.doc.drawContents(p)
